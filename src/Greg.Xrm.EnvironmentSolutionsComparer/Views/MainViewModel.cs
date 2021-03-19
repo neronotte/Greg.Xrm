2c575595daa6b67131fe437293d9ebc9f327e3ed
_neronotte <riccardo.gregori@outlook.com>
2021-03-19 10:15:53 +0100

    _n.EnvironmentSolutionComparer: - Replaced the distinct by EnvironmentId with a distinct by ConnectionId. This should resolve the mismatching errors

--- src/Greg.Xrm.EnvironmentSolutionsComparer/Views/MainViewModel.cs
+++ src/Greg.Xrm.EnvironmentSolutionsComparer/Views/MainViewModel.cs
@@ -38,7 +38,7 @@ namespace Greg.Xrm.EnvironmentSolutionsComparer.Views
 
 
 			// if the current environment is still there, removeit
-			var oldCurrentEnvironment = this.EnvironmentList.FirstOrDefault(_ => string.Equals( _.Detail.EnvironmentId, connectionDetail.EnvironmentId));
+			var oldCurrentEnvironment = this.EnvironmentList.FirstOrDefault(_ => string.Equals( _.Detail.ConnectionId, connectionDetail.ConnectionId));
 			if (oldCurrentEnvironment != null)
 			{
 				this.EnvironmentList.Remove(oldCurrentEnvironment);
@@ -56,7 +56,7 @@ namespace Greg.Xrm.EnvironmentSolutionsComparer.Views
 
 		public void AddEnvironment(ConnectionDetail connectionDetail, IOrganizationService crm = null)
 		{
-			var oldCurrentEnvironment = this.EnvironmentList.FirstOrDefault(_ => string.Equals(_.Detail.EnvironmentId, connectionDetail.EnvironmentId));
+			var oldCurrentEnvironment = this.EnvironmentList.FirstOrDefault(_ => string.Equals(_.Detail.ConnectionId, connectionDetail.ConnectionId));
 			if (oldCurrentEnvironment != null)
 			{
 				this.EnvironmentList.Remove(oldCurrentEnvironment);
